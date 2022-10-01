d83531f04d695aa6bc1d64fb7050352d06b87646
MarkVed17 <vedantlahane17@gmail.com>
2022-10-01 20:22:34 +0530

    added `Sidebar` to `Main` layout

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Header } from "../Header/Header";
+import { Sidebar } from "../../components";
 
 const Main = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
 };
 
 export { Main };
