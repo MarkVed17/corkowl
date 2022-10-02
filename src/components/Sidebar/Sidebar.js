4b84b6b3bf2af959ec4d02644045b04b9ff35833
MarkVed17 <vedantlahane17@gmail.com>
2022-10-03 04:18:50 +0530

    added a `Footer` to `Sidebar`

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,16 +13,22 @@ const Sidebar = () => {
 
   return (
     <aside className="sidebar">
-      {sidebarLinks.map(({ url, icon, name }) => (
-        <a
-          key={name}
-          href={url} // href just for static page but would be a React Router <NavLink> after adding react-router
-          className={url === "/" ? "sidebar-link-active" : "sidebar-link"}
-        >
-          <img src={icon} alt={name} />
-          <p>{name}</p>
-        </a>
-      ))}
+      <nav>
+        {sidebarLinks.map(({ url, icon, name }) => (
+          <a
+            key={name}
+            href={url} // href just for static page but would be a React Router <NavLink> after adding react-router
+            className={url === "/" ? "sidebar-link-active" : "sidebar-link"}
+          >
+            <img src={icon} alt={name} />
+            <p>{name}</p>
+          </a>
+        ))}
+      </nav>
+      <footer className="footer">
+        <img src="/assets/footer-logo.svg" alt="logo" />
+        <p className="footer-copyright">© 2020 CorkOwl, All Rights Reserved.</p>
+      </footer>
     </aside>
   );
 };
